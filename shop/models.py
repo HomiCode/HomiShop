42bb26ace54e5eaaed71165d94431d44066ef4ef
amirhomayooon <amirhomayoonrefaei@gmail.com>
2023-02-17 00:15:29 +0330

    Start Models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+        def __str__(self):
+            return self.name
