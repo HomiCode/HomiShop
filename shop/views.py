212ccdf10270e228de2d4c14cf823320cff573c0
amirhomayooon <amirhomayoonrefaei@gmail.com>
2023-02-18 09:12:13 +0330

    product detail

--- shop/views.py
+++ shop/views.py
@@ -13,3 +13,7 @@ def product_list(request, category_slug=None):
                 {'category': category,
                 'categories': categories,
                 'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'shop/product/detail.html', {'product': product})
